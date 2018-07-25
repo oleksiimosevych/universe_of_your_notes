2e691947509d7815ee6f4b899adbe0a568696f69
oleksiimosevych <regosso@ukr.net>
2018-07-25 16:06:55 +0300

    we can register on users/sign_up and create a post

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+#just added relations
+  has_many :posts
 end
