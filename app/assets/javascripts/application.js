12d0a81b0f19412d846cccb437c95ec9abf2e263
oleksiimosevych <regosso@ukr.net>
2018-08-16 15:42:02 +0300

    text looks pretty good but font need to be changed. edit works good

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,35 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+$(function() {
+    // Define data source for At.JS.
+    var datasource = ["Jacob", "Isabella", "Ethan", "Emma", "Michael", "Olivia" ];
+ 
+    // Build data to be used in At.JS config.
+    var names = $.map(datasource, function (value, i) {
+      return {
+        'id': i, 'name': value, 'email': value + "@email.com"
+      };
+    });
+ 
+    // Define config for At.JS.
+    var config = {
+      at: "@",
+      data: names,
+      displayTpl: '<li>${name} <small>${email}</small></li>',
+      limit: 200
+    }
+ 
+    // Initialize editor.
+    $('#froala-editor')
+      .on('froalaEditor.initialized', function (e, editor) {
+        editor.$el.atwho(config);
+ 
+      	editor.events.on('keydown', function (e) {
+          if (e.which == $.FroalaEditor.KEYCODE.ENTER && editor.$el.atwho('isSelecting')) {
+            return false;
+          }
+        }, true);
+      })
+      .froalaEditor()
+  });
